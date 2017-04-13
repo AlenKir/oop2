81fecbf077c26eb1b6016a4df019ea6fcfe9c01b
AlenKir <kirillova.alena@gmail.com>
2017-04-13 10:56:37 +0300

    1

---
+++ shape.hpp
@@ -0,0 +1,15 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+#include "screen.hpp"
+class Shape
+{
+public:
+	virtual void draw(Screen* screen) const = 0;
+	virtual void move(Point p) = 0;
+	virtual Point getLeftTop() const = 0;
+	virtual Point getRightTop() const = 0;
+	virtual Point getLeftBottom() const = 0;
+	virtual Point getRightBottom() const = 0;
+	virtual ~Shape() {}
+};
+#endif //SHAPE_HPP
