1479685d51f6b37fa4c071d7f84c6804894e39a8
AlenKir <kirillova.alena@gmail.com>
2017-04-14 17:49:23 +0300

    добавлено вычисление креста

--- crossedsquare.hpp
+++ crossedsquare.hpp
@@ -1,47 +1,83 @@
 #include "square.hpp"
 #include "cross.hpp"
-
-class CrossedSquare : public virtual Shape, public virtual Cross
+#include <cstdint>
+#include <cmath>
+#include <vector>
+#include <memory>
+class CrossedSquare : public virtual Square, public virtual Cross
 {
-
-	CrossedSquare(const Point& leftTop, const Point& rightBottom, const Point& left, const Point& top)
+public:
+	CrossedSquare(const Point& leftTop, const Point& rightBottom/*, const Point& left, const Point& top*/)
 	{
+		/*Square::Square(leftTop, rightBottom);*/
 		_left = new Line(leftTop, Point(leftTop.getX(), rightBottom.getY()));
 		_top = new Line(leftTop, Point(rightBottom.getX(), leftTop.getY()));
 		_right = new Line(Point(rightBottom.getX(), leftTop.getY()), rightBottom);
 		_bottom = new Line(Point(leftTop.getX(), rightBottom.getY()), rightBottom);
 
+		
+		Point left = coordCrossLeft(leftTop, rightBottom);
+		Point top = coordCrossTop(leftTop, rightBottom);
+
 		Point p1, p2;
 		Point p3, p4;
 		p1 = left;
-		p2 = Point(top.getX() + top.getX() - left.getX(), left.getY());
+		p2 = coordCrossRight(leftTop, rightBottom);
+		//p2 = Point(top.getX() + top.getX() - left.getX(), left.getY());
 		p3 = top;
-		p4 = Point(top.getX(), left.getY() - top.getY() + left.getY());
+		//p4 = Point(top.getX(), left.getY() - top.getY() + left.getY());
+		p4 = coordCrossBttm(leftTop, rightBottom);
 		_first = new Line(p1, p2);
 		_second = new Line(p3, p4);
 	}
 
+	Point coordCrossLeft(const Point& leftTop, const Point& rightBottom)
+	{
+		int distance = rightBottom.getY() - leftTop.getY();
+		Point l = Point(leftTop.getX(), leftTop.getY() + distance / 2);
+		return l;
+	}
+	Point coordCrossTop(const Point& leftTop, const Point& rightBottom)
+	{
+		int distance = rightBottom.getX() - leftTop.getX();
+		Point t = Point(leftTop.getX() + distance / 2, leftTop.getY());
+		return t;
+	}
+	Point coordCrossBttm(const Point& leftTop, const Point& rightBottom)
+	{
+		int distance = rightBottom.getX() - leftTop.getX();
+		distance /= 2;
+		Point bttm = Point(leftTop.getX() + distance, rightBottom.getY());
+		return bttm;
+	}
+	Point coordCrossRight(const Point& leftTop, const Point& rightBottom)
+	{
+		int distance = rightBottom.getY() - leftTop.getY();
+		distance /= 2;
+		Point bttm = Point(rightBottom.getX(), leftTop.getY()+distance);
+		return bttm;
+	}
+
 	virtual void draw(Screen* screen) const
 	{
-		//Square::draw(screen);
+		std::cout << "crossedsquare draw" << std::endl;
 		_left->draw(screen);
 		_top->draw(screen);
 		_right->draw(screen);
 		_bottom->draw(screen);
-
-		//Cross::draw(screen);
+		
 		_first->draw(screen);
 		_second->draw(screen);
 	}
 	virtual void move(Point p)
 	{
-		//Square::move(p);
+	
 		_left->move(p);
 		_top->move(p);
 		_right->move(p);
 		_bottom->move(p);
 
-		//Cross::move(p);
+		
 		_first->move(p);
 		_second->move(p);
 	}
